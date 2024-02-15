b128a7903055013b9e01ed21aa9e4b0bee255002
wendao.yang <wendao.yang@shopee.com>
2024-02-15 21:41:29 +0800

    feat: add title

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -6,6 +6,7 @@ const MainHeader: React.FC = () => {
   return (
     <Header style={{ display: 'flex', alignItems: 'center' }}>
       <div className="logo" />
+      <h1>A project of interest</h1>
     </Header>
   );
 }
